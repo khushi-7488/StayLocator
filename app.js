b47e42670144e44a06a85d9fc30bf0ad0906ecd2
khushi-7488 <khushiitiwari88@gmail.com>
2025-09-20 14:33:46 +0530

    reviews

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js")
 const session = require("express-session");
 const flash = require("connect-flash")
+const passport = require("passport")
+const LocalStrategy = require("passport-local")
+const User = require("./models/user.js")
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "/views"))
@@ -33,16 +36,35 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use((req, res, next)=>{
+
+app.use(passport.initialize())
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.authenticate());
+passport.deserializeUser(User.deserializeUser());
+
+
+
+app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     next();
 })
 
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "khushi@gmail.com",
+//         username: "khushi"
+//     })
+//    let registeredUser = await User.register(fakeUser,"helloworld");
+// res.send(registeredUser);
+// })
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main().then(() => {
@@ -56,8 +78,9 @@ async function main() {
 
 
 
-app.use("/listings", listings)
-app.use("/listings/:id/reviews", reviews)
+app.use("/listings", listingRouter)
+app.use("/listings/:id/reviews", reviewRouter)
+app.use("/", userRouter)
 
 app.all(/.*/, (req, res, next) => {
     next(new ExpressError(404, "page not found"))
