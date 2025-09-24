b12bb70ccecfc93ab3e6cebda7c28099c10a01de
khushi-7488 <khushiitiwari88@gmail.com>
2025-09-24 23:56:50 +0530

    deployment

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ app.all(/.*/, (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "something went wrong" } = err;
-    res.status(statusCode).render("error.ejs", { message })
+    res.status(statusCode).render("error", { message })
 })
 
 app.listen(8080, () => {
